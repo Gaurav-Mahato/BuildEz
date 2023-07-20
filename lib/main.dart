8a4ac01b93ca677e2f992eb4ef85adc67276569f
GauravMahato <Gauravmahato1309@gmail.com>
2023-07-20 23:45:46 +0530

    Added Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buildez/features/authentication/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,22 +19,15 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark
       ),
       themeMode: ThemeMode.system,
-      home: const AppHome()
+      home: const WelcomeScreen()
     );
   }
 }
- class AppHome extends StatelessWidget{
-  const AppHome({Key ? key}) : super(key:key);
-
-  @override
-   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My First Application')
-      ),
-      body: const Center(
-        child: Text('Hello World')
-      )
-    );
-  }
- }
+ // class AppHome extends StatelessWidget{
+ //  const AppHome({Key ? key}) : super(key:key);
+ //
+ //  @override
+ //   Widget build(BuildContext context){
+ //    return const WelcomeScreen();
+ //  }
+ // }
