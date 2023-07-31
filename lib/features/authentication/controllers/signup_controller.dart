e495f54d242c421673363e5a15e6737faf4e78ab
GauravMahato <Gauravmahato1309@gmail.com>
2023-07-31 17:10:28 +0530

    FireStore create functionality

--- lib/features/authentication/controllers/signup_controller.dart
+++ lib/features/authentication/controllers/signup_controller.dart
@@ -1,8 +1,11 @@
 
 
 import 'package:buildez/features/repository/authentication_repository.dart';
+import 'package:buildez/features/repository/user_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+
+import '../models/user_model.dart';
 
 class SignUpController extends GetxController{
   static SignUpController get instance => Get.find();
@@ -12,7 +15,17 @@ class SignUpController extends GetxController{
   final name = TextEditingController();
   final password = TextEditingController();
 
+  final userRepo = Get.put(UserRepository());
+
   void registerUser(String email,String password){
-    AuthenticationRepository.instance.createUserWithEmailAndPassword(email, password);
+    String? error = AuthenticationRepository.instance.createUserWithEmailAndPassword(email, password) as String?;
+    if(error != null){
+      Get.showSnackbar(GetSnackBar(message: error.toString()));
+    }
+  }
+
+  Future<void> createUser(UserModel user) async{
+    await userRepo.createUser(user);
+    registerUser(user.email, user.password);
   }
 }
